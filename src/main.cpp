e1aea3e49c0861ee68149318ec7733b926bd5f64
Sola_akw <sola_akw@outlook.com>
2023-08-11 15:18:41 +1200

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,41 @@
 #include "MyApp.h"
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdio> 
+#include <unistd.h>
+#include <vector>
+char *getcwd(char *buf, size_t size);
 
-int main() {
-  MyApp app;
-  app.Run();
+vector<string> recipe;
 
+
+using namespace std;
+
+int main()
+{
+
+  FILE* ptr; //file location after parsing
+    char ch; //
+  
+    ptr = fopen("src/DummyData.txt", "r");
+ 
+    if (NULL == ptr) {
+        perror("Error");
+    }
+  
+    // Printing what is written in file
+    // character by character using loop.
+    do {
+      while(ch != '\n'){
+        ch = fgetc(ptr);
+        printf("%c", ch); 
+      }
+    } while (ch != EOF);
+ 
+    // Closing the file
+    fclose(ptr);
+  
   return 0;
 }
