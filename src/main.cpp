4bec2f84b5f0f00e09c052bdb943375c6c1d429d
josh-10110 <109562964+josh-10110@users.noreply.github.com>
2023-08-19 11:37:29 +1200

    classes to create a db instance of a recipe object

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,15 @@
 #include "MyApp.h"
 #include "databaseConnection.h"
+#include "recipeDatabase.h"
 
 int main()
 {
+    // Example of using the RecipeDatabase class to create an instance of a Recipe object 
+    // and return its value
 
-    // Example of using the database connection and querying the database (can be deleted)
-    DatabaseConnection dbConn;
-    try {
-        auto con = dbConn.getConnection();
-        std::unique_ptr<sql::Statement> stmt(con->createStatement());
-        stmt->execute("USE PantryPro;");
-        std::unique_ptr<sql::ResultSet> res(stmt->executeQuery("SELECT * FROM recipes;"));
-        while (res->next()){
-            std::cout << "Recipe Name: " << res->getString("recipeName") << std::endl;
-        }
-    } catch (sql::SQLException &e){
-        std::cout << "# ERR: SQLException in " << __FILE__ << " on line " << __LINE__ << std::endl;
-        std::cout << "# ERR: " << e.what();
-        std::cout << " (MySQL error code: " << e.getErrorCode();
-        std::cout << ", SQLState: " << e.getSQLState() << " )" << std::endl;
-        return EXIT_FAILURE;
-    }
+    RecipeDatabase db;
+    Recipe myRecipe = db.getRecipeById(38);
+    std::cout << "Recipe Name: " << myRecipe.getName() << std::endl;
 
     MyApp app;
     app.Run();
