082b9c793c5e9907780564920cfe971a59b21c30
Luke <lukewebb2000@gmail.com>
2023-08-18 22:46:13 +1200

    Basic Recipe Class and related tests

---
+++ tests/RecipeTests.cpp
@@ -0,0 +1,41 @@
+#include "../src/Recipe.cpp"
+#include "catch_amalgamated.cpp"
+
+/**
+ * @brief First test case for Recipe objects
+ *  Testing Constructors, setters and getters.
+ */
+TEST_CASE( "Check all methods", "[classic]" )  
+{
+    SECTION("Tests that the constructor works correctly"){
+        Recipe testRecipe1(1);
+        REQUIRE(testRecipe1.getRecipeID() == 1);
+        Recipe testRecipe2(10202);
+        REQUIRE(testRecipe2.getRecipeID() == 10202);
+        Recipe testRecipe3(777);
+        REQUIRE(testRecipe3.getRecipeID() == 777);
+    }
+
+    SECTION("Positive tests check the setters and getters work correctly"){
+        Recipe testRecipe1(1);
+        testRecipe1.setName("aaa");
+        testRecipe1.setPrepTime("PT10M");
+        testRecipe1.setTotalTime("PT10H");
+        testRecipe1.setCategory("bbb");
+        testRecipe1.setCalories(1000);
+        REQUIRE(testRecipe1.getName() == "aaa");
+        REQUIRE(testRecipe1.getPrepTime() == "PT10M");
+        REQUIRE(testRecipe1.getTotalTime() == "PT10H");
+        REQUIRE(testRecipe1.getCategory() == "bbb");
+        REQUIRE(testRecipe1.getCalories() == 1000);
+    }
+
+    SECTION("Negative test, where applicable"){
+        Recipe testRecipe1(1);
+        testRecipe1.setCalories(1000);
+        REQUIRE(testRecipe1.getCalories() != 100);
+        REQUIRE(testRecipe1.getCalories() != 10000);
+        REQUIRE(testRecipe1.getCalories() != 1);
+    }
+    
+}
