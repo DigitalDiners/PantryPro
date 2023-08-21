bd021a7f29565ca2abdae2ff505707d60e1da455
josh-10110 <109562964+josh-10110@users.noreply.github.com>
2023-08-22 09:32:23 +1200

    added comments for databaseConnection.cpp

--- src/databaseConnection.cpp
+++ src/databaseConnection.cpp
@@ -1,6 +1,11 @@
 #include "databaseConnection.h"
 
-void DatabaseConnection::loadCredentialsFromINI() {
+/**
+ * @brief Load database credentials from environment variables.
+ * 
+ * Private method to populate the uri, user, and password member variables.
+ */
+void DatabaseConnection::loadCredentialsFromENV() {
     const char* tempUri = std::getenv("DB_IP");
     const char* tempUser = std::getenv("DB_USER");
     const char* tempPass = std::getenv("DB_PASS");
@@ -10,12 +15,26 @@ void DatabaseConnection::loadCredentialsFromINI() {
     password = (tempPass) ? tempPass : "";
 }
 
+/**
+ * @brief Constructs a new DatabaseConnection object.
+ * 
+ * Initializes the SQL driver instance and loads the database credentials from environment variables.
+ * Sets the default database name to "PantryPro".
+ */
 DatabaseConnection::DatabaseConnection() 
 : driver(sql::mysql::get_driver_instance()) {
-    loadCredentialsFromINI();
+    loadCredentialsFromENV();
     database = "PantryPro";
 }
 
+/**
+ * @brief Establishes and returns a connection to the database.
+ * 
+ * Uses the MySQL driver to connect using the loaded credentials. Sets the connection schema to 
+ * the default database name.
+ * 
+ * @return A pointer to the established connection.
+ */
 std::unique_ptr<sql::Connection> DatabaseConnection::getConnection() {
     auto connection = std::unique_ptr<sql::Connection>(driver->connect(uri, user, password));
     connection->setSchema(database);
