f9c5415c400628060dafbb16300b432a6df60a56
josh-10110 <109562964+josh-10110@users.noreply.github.com>
2023-08-22 09:32:38 +1200

    added comments for databaseConnection.h

--- src/databaseConnection.h
+++ src/databaseConnection.h
@@ -4,6 +4,13 @@
 #include <memory>
 #include <mysql/jdbc.h>
 
+/**
+ * @class DatabaseConnection
+ * @brief Manages database connections using MySQL.
+ * 
+ * This class is responsible for establishing and returning MySQL connections. 
+ * It loads credentials from environment variables to facilitate connections.
+ */
 class DatabaseConnection {
 private:
     std::string uri;
@@ -12,9 +19,31 @@ private:
     std::string database;
     sql::Driver* driver;
 
-    void loadCredentialsFromINI();
+    /**
+     * @brief Load database credentials from environment variables.
+     * 
+     * This method reads the DB_IP, DB_USER, and DB_PASS environment variables 
+     * to set the uri, user, and password members, respectively.
+     */
+    void loadCredentialsFromENV();
 
 public:
+
+    /**
+     * @brief Default constructor.
+     * 
+     * Instantiates the DatabaseConnection object and loads credentials from environment variables.
+     * Also initializes the database name as "PantryPro".
+     */
     DatabaseConnection();
+
+    /**
+     * @brief Establishes and returns a connection to the database.
+     * 
+     * Uses the MySQL driver to connect using the loaded credentials. Sets the connection schema to 
+     * the default database name.
+     * 
+     * @return A pointer to the established connection.
+     */
     std::unique_ptr<sql::Connection> getConnection();
 };
