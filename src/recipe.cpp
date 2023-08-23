bf116dd2fcaf7bc3f68bfa0d4c331ab18a2b405b
Sola <sola_akw@outlook.com>
2023-08-23 20:32:24 +1200

    recipeee

--- src/recipe.cpp
+++ src/recipe.cpp
@@ -1,4 +1,4 @@
-#include "Recipe.h"
+#include "recipe.h"
 
 Recipe::Recipe(int id, const std::string& name, int aId, const std::string& aName,
                int cTime, int pTime, int tTime, const std::string& dPublished,
