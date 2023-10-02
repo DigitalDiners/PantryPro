97e95545c72ae0d1d0bdd0c9be1adc6c7f0ba4ca
sola <sola_akw@outlook.com>
2023-10-02 21:54:08 +1300

    working on it more

--- src/MealPlanner.cpp
+++ src/MealPlanner.cpp
@@ -1,48 +1,28 @@
-#include "json.hpp"
 #include "mealPlanner.h"
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <filesystem>
+#include "recipe.h"
 
-using json = nlohmann::json;
-std::string plannerJson;
-std::vector<Day> plannerVector;
 RecipeObject hold = RecipeObject(0);
+std::vector<std::vector<int> > recipeVector;
 
 MealPlanner::MealPlanner()
 {
-    // std::cout << "Constructing meal planner..." << std::endl;
-    Day monday =  Day(0);
-    Day tuesday =  Day(1);
-    Day wednesday =  Day(2);
-    Day thursday =  Day(3);
-    Day friday =  Day(4);
-    Day saturday =  Day(5);
-    Day sunday =  Day(6);
-    initialiseDays(monday);
-    initialiseDays(tuesday);
-    initialiseDays(wednesday);
-    initialiseDays(thursday);
-    initialiseDays(friday);
-    initialiseDays(saturday);
-    initialiseDays(sunday);
-    plannerVector.push_back(monday);
-    plannerVector.push_back(tuesday);
-    plannerVector.push_back(wednesday);
-    plannerVector.push_back(thursday);
-    plannerVector.push_back(friday);
-    plannerVector.push_back(saturday);
-    plannerVector.push_back(sunday);
-    std::cout << plannerVector.size() << std::endl;
-    std::cout << plannerVector[0].getLunch() << std::endl;
+    // std::vector<Recipe> monday(4);
+    // std::vector<Recipe> tuesday(4);
+    // std::vector<Recipe> wednesday(4);
+    // std::vector<Recipe> thursday(4);
+    // std::vector<Recipe> friday(4);
+    // std::vector<Recipe> saturday(4);
+    // std::vector<Recipe> sunday(4);
+    // std::vector<std::vector<Recipe> > recipeVector = {monday, tuesday, wednesday, thursday, friday, saturday, sunday};
+    std::vector<std::vector<int> > recipeVector(7, std::vector<int>(3));
+    std::cout << recipeVector.size() << std::endl;
+    std::cout << recipeVector.size() << std::endl;
 }
 
-void MealPlanner::initialiseDays(Day day){
-    day.setMeal(hold,0);
-    day.setMeal(hold,1);
-    day.setMeal(hold,2);
-}
 
 bool MealPlanner::addToPlanner(std::string recipeName, int recipeId, std::string day, std::string meal)
 {
@@ -50,52 +30,29 @@ bool MealPlanner::addToPlanner(std::string recipeName, int recipeId, std::string
 
     int dayNum;
     int mealNum;
-    std::string mealID;
-    std::string mealName;
-    std::string recipeID = std::to_string(recipeId);
-
+    // std::string recipeID = std::to_string(recipeId);
     // RecipeDatabase recipeDB;
     // Recipe addRecipe = recipeDB.getRecipeById(recipeId);
-    RecipeObject addRecipe = RecipeObject(recipeId);
     dayNum = getDayNum(day);
-    std::cout << "recipeobject made" << std::endl;
+    std::cout << "recipe made" << std::endl;
     if (meal == "Breakfast")
     {
         mealNum=0;
-        mealName = "breakfast";
-        mealID = "breakfastID";
     }
     else if (meal == "Lunch")
     {
         mealNum=1;
-        mealName = "lunch";
-        mealID = "lunchID";
     }
     else if (meal == "Dinner")
     {
         mealNum=2;
-        mealName = "dinner";
-        mealID = "dinnerID";
     }
     else if (meal == "Snack")
     {
         mealNum=3;
-        mealName = "snack";
-        mealID = "snackID";
     }
+    recipeVector[dayNum][mealNum] = recipeId;
 
-    std::cout << plannerVector[0].getBreakfast() << std::endl;
-try {
-    plannerVector[dayNum].setMeal(addRecipe, mealNum);
-  // Code that you want to try to execute
-    std::cout << "plannervector found" << std::endl;
-    std::cout << "day and num set " << dayNum << "    " << mealName << std::endl;
-} catch (std::exception) {
-    std::cout << "plannervector fail" << std::endl;
-  // Code that you want to execute if an exception occurs
-}
-    // Day newMeal =  *addMeal;
-    // newMeal.setMeal(*addRecipe, mealNum);
     return true;
 
 }
@@ -138,21 +95,11 @@ MealPlanner::~MealPlanner()
     delete[] days; // Clean up the dynamically allocated memory
 }
 
-Day &MealPlanner::getDay(int dayNumber)
-{
-    if (dayNumber > 6 || dayNumber < 0)
-    {
-        std::cout << "Index out of range! Returning monday." << std::endl;
-        return days[0];
-    }
-    else
-    {
-        return days[dayNumber];
-    }
-}
 
 std::string MealPlanner::getPlannerJson() const
 {
+    std::string plannerJson;
+    
     std::cout << "Get Planner json called" << std::endl;
     return plannerJson;
 }
